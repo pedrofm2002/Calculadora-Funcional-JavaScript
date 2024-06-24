b82cb4ad6f50c40b7280dc8960ea625d3ab2baa9
pedrof2002 <pedroferrarimartinez12@gmail.com>
2024-06-24 12:56:19 -0300

    Calculator In JavaScript

---
+++ js/index.js
@@ -0,0 +1,16 @@
+import calculate from "./calculate.js";
+import copyToClipboard from "./copyToClipboard.js";
+import { handleButtonPress, handleClear, handleTyping } from "./keyHandlers.js";
+import switchTheme from "./themeSwitcher.js";
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", handleButtonPress);
+});
+
+document.getElementById("input").addEventListener("keydown", handleTyping);
+document.getElementById("clear").addEventListener("click", handleClear);
+document.getElementById("equal").addEventListener("click", calculate);
+document
+  .getElementById("copyToClipboard")
+  .addEventListener("click", copyToClipboard);
+document.getElementById("themeSwitcher").addEventListener("click", switchTheme);
